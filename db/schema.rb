ba4a12b4ae95294d39e971090065e67bc1e8b4a8
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2017-03-02 16:36:30 +0000

    continue progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302154508) do
+ActiveRecord::Schema.define(version: 20170302160042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.string   "email"
+    t.boolean  "is_leader"
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "group_size"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.text     "description"
+    t.boolean  "ongoing"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "things", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.text     "description"
+    t.integer  "price_range"
+    t.integer  "city_id"
+    t.string   "thing_type"
+    t.string   "link"
+    t.text     "tag"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
