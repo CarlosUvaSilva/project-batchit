dbbc80e076acd2cc9c187a13ee3a2d9d6464a268
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2017-03-08 17:29:32 +0000

    Removed messages and related chatbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307173011) do
+ActiveRecord::Schema.define(version: 20170307110248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 20170307173011) do
     t.datetime "updated_at", null: false
     t.float    "latitude"
     t.float    "longitude"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "participants", force: :cascade do |t|
@@ -104,5 +96,4 @@ ActiveRecord::Schema.define(version: 20170307173011) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
   end
 
-  add_foreign_key "messages", "users"
 end
