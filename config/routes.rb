66486f116c32caa21c82a1ee27fada04559d7395
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2017-03-17 11:07:56 +0100

    made the search work having everything in 1 page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'errors/not_found'
 
   get 'errors/internal_server_error'
+  resources :cities, only: [:index]
 
 
   match "/404", :to => "errors#not_found", :via => :all
